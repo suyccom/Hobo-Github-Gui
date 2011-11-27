efd8d400fd380e8fc5e4559a220ffbe841022a18
Ignacio Huerta <ignacio@ihuerta.net>
2011-11-27 10:51:10 +0100

    Detalle para exportacion a tracks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include Hobo::Controller::AuthenticationSupport
   before_filter :except => [:login, :forgot_password, :accept_invitation, :do_accept_invitation, :reset_password,
-:do_reset_password] do
+:do_reset_password, :exportar_tracks] do
      login_required unless Usuario.count == 0
   end
 end
